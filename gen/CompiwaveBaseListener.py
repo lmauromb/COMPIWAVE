09538a33d6a0bdfea6d6099e42b395186f003746
Luis Mauro Martínez Báez <mauromb3@gmail.com>
2017-03-21 00:53:25 -0600

    Created: class for populating the Symbol Talbe

--- gen/CompiwaveBaseListener.py
+++ gen/CompiwaveBaseListener.py
@@ -1,6 +1,82 @@
 from CompiwaveListener import *
+from helpClasess import *
 
 class CompiwaveBaseListener(CompiwaveListener):
-    pass
+    currentScope = None
+    symbolTable = SymbolTable()
 
-print(help(CompiwaveBaseListener))
+    def enterCompiwave(self, ctx:CompiwaveParser.CompiwaveContext):
+        self.currentScope = GlobalScope()
+        self.symbolTable.globals = self.currentScope
+
+    def exitCompiwave(self, ctx:CompiwaveParser.CompiwaveContext):
+        print(self.symbolTable)
+
+
+    def enterBlock(self, ctx:CompiwaveParser.BlockContext):
+        self.currentScope = LocalScope(self.currentScope)
+
+    def exitBlock(self, ctx:CompiwaveParser.BlockContext):
+        s = "{}: {}".format(self.currentScope.getScopeName() ,self.currentScope)
+        print(s)
+        # print(self.currentScope.enclosingScope.getScopeName())
+        self.currentScope = self.currentScope.enclosingScope
+
+
+    def enterFunction_declaration(self, ctx:CompiwaveParser.Function_declarationContext):
+        if ctx.cwtype() is None:
+            t = "void"
+        else:
+            t = ctx.cwtype().getText()
+
+        s = "def fun {} as type {}".format(
+            ctx.ID().getText(), t
+        )
+        # print(s)
+        returnType = t
+        ms = MethodSymbol(ctx.ID().getText(), returnType, self.currentScope)
+        if self.currentScope.resolve(ms) is None:
+            self.currentScope.define(ms)
+            self.currentScope = ms
+        else:
+            raise Exception("Name {} already used".format(ctx.ID().getText()))
+
+    def exitFunction_declaration(self, ctx:CompiwaveParser.Function_declarationContext):
+        s = "args: {}".format(self.currentScope)
+        # print(s)
+        self.currentScope = self.currentScope.enclosingScope
+
+
+    def enterMain_function(self, ctx:CompiwaveParser.Main_functionContext):
+        ms = MethodSymbol("main", "void", self.currentScope)
+        self.currentScope.define(ms)
+        self.currentScope = ms
+
+    def exitMain_function(self, ctx:CompiwaveParser.Main_functionContext):
+        self.currentScope = self.currentScope.enclosingScope
+
+
+    def enterFunction_param(self, ctx:CompiwaveParser.Function_paramContext):
+        vs = VariableSymbol(ctx.ID().getText(), ctx.cwtype().getText())
+        self.currentScope.define(vs)
+
+
+    def enterVar_declaration(self, ctx:CompiwaveParser.Var_declarationContext):
+        s = "def var: {} as type {}".format(ctx.ID().getText(),
+                                            ctx.cwtype().getText())
+        # print(s)
+        vs = VariableSymbol(ctx.ID().getText(), ctx.cwtype().getText())
+        if self.currentScope.resolve(vs) is None:
+            self.currentScope.define(vs)
+        else:
+            raise Exception("Name {} already used".format(ctx.ID().getText()))
+
+
+    def enterList_declaration(self, ctx:CompiwaveParser.List_declarationContext):
+        vs = VariableSymbol(ctx.ID().getText(), ctx.cwtype().getText())
+        if self.currentScope.resolve(vs) is None:
+            self.currentScope.define(vs)
+        else:
+            raise Exception("Name {} already used".format(ctx.ID().getText()))
+
+
